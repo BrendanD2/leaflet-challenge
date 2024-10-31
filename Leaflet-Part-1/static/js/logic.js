c9cceb713a0f2990e3d7c4bf5284f61b7c4586f4
BrendanD2 <brendandoucette2021@gmail.com>
2024-10-30 21:58:55 -0400

    Updates

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,34 +1,45 @@
-let myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-
   // Set Geojson url to a variable
-  let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+// let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-  // Retrieve the data and get the necessary data values
-  d3.json(url).then(function (data){
-    console.log(data)
-  });
+//   // Perform a GET request to the query URL/
+// d3.json(url).then(function (data) {
+//   // Once we get a response, send the data.features object to the createFeatures function.
+//   createMarkers(data.features);
+// });
 
-  function createFeatures(earthquakeData) {
 
-    // Define a function that we want to run once for each feature in the features array.
-    // Give each feature a popup that describes the magnitude "mag", location "place", and depth "dmin" of the earthquake.
-    function onEachFeature(feature, layer) {
-      layer.bindPopup(`<h3>${feature.properties.mag}</h3><hr><p>${new Date(feature.properties.place)}</h3><hr>${new Date(feature.properties.dmin)}</p>`);
-    };
-     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+// function createMarkers(earthquakeData) {
+//   console.log("each earthquake")
+//   console.log(earthquakeData)
+let myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-};
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+
+d3.json(url).then(function(response) {
+  console.log(response)
+  let earthquakeData = response.features
+  console.log(earthquakeData)
+  for (let index = 0; index < earthquakeData.length; index++) {
+    let earthquake = earthquakeData[index];
+    console.log(earthquake.geometry.coordinates)
+
+    let marker = L.circleMarker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], {
+      color: earthquake.geometry.coordinates[2], // Circle color
+       // HEX color code
+      fillOpacity: 0.7, // Set the fill opacity 
+      radius: earthquake.properties.mag * 5
+    }).addTo(myMap)
+
+    marker.bindPopup(`Magnitude: ${earthquake.properties.mag}<hr> Place: ${(earthquake.properties.place)}<hr> Depth: ${(earthquake.properties.dmin)}<hr>`);
+
+  }
+});
+ 
